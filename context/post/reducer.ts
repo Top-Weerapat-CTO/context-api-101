f19dffeb8cc27636d5abec98255be59f2f041ab1
Top-Weerapat-CTO <weerapat@central.tech>
2021-07-22 17:42:51 +0700

    use Imutable data

--- context/post/reducer.ts
+++ context/post/reducer.ts
@@ -12,53 +12,86 @@ export const initialState = {
 
 export const postReducer = (state, action) => {
   switch (action.type) {
+    // case "increment":
+    //   return { ...state, page: state.page + 1 };
+    // case "decrement":
+    //   return { ...state, page: state.page - 1 };
+    // case "reset":
+    //   return { ...state, page: 0 };
+    // case FETCH_POST_BY_ID_REQUEST:
+    //   return {
+    //     ...state,
+    //     keys: {
+    //       ...state.keys,
+    //       [action.key]: {
+    //         ...state.keys?.[action.key],
+    //         isFetching: false,
+    //         loading: true,
+    //         error: false,
+    //       },
+    //     },
+    //   };
+    // case FETCH_POST_BY_ID_SUCCESS:
+    //   return {
+    //     ...state,
+    //     keys: {
+    //       ...state.keys,
+    //       [action.key]: {
+    //         ...state.keys?.[action.key],
+    //         isFetching: true,
+    //         loading: false,
+    //         data: action.data,
+    //         error: false,
+    //       },
+    //     },
+    //   };
+    // case FETCH_POST_BY_ID_FAILURE:
+    //   return {
+    //     ...state,
+    //     keys: {
+    //       ...state.keys,
+    //       [action.key]: {
+    //         ...state.keys?.[action.key],
+    //         isFetching: false,
+    //         loading: false,
+    //         error: action?.error?.message,
+    //       },
+    //     },
+    //   };
+
     case "increment":
-      return { ...state, page: state.page + 1 };
+      state.page += 1;
+      return;
     case "decrement":
-      return { ...state, page: state.page - 1 };
+      state.page -= 1;
+      return;
     case "reset":
-      return { ...state, page: 0 };
+      state.page = 0;
+      return;
     case FETCH_POST_BY_ID_REQUEST:
-      return {
-        ...state,
-        keys: {
-          ...state.keys,
-          [action.key]: {
-            ...state.keys?.[action.key],
-            isFetching: false,
-            loading: true,
-            error: false,
-          },
-        },
-      };
+      if (!state.keys[action.key]) {
+        state.keys[action.key] = {
+          isFetching: false,
+          loading: true,
+          error: false,
+        };
+      } else {
+        state.keys[action.key].isFetching = false;
+        state.keys[action.key].loading = true;
+        state.keys[action.key].error = false;
+      }
+      return;
     case FETCH_POST_BY_ID_SUCCESS:
-      return {
-        ...state,
-        keys: {
-          ...state.keys,
-          [action.key]: {
-            ...state.keys?.[action.key],
-            isFetching: true,
-            loading: false,
-            data: action.data,
-            error: false,
-          },
-        },
-      };
+      state.keys[action.key].isFetching = true;
+      state.keys[action.key].loading = false;
+      state.keys[action.key].error = false;
+      state.keys[action.key].data = action.data;
+      return;
+
     case FETCH_POST_BY_ID_FAILURE:
-      return {
-        ...state,
-        keys: {
-          ...state.keys,
-          [action.key]: {
-            ...state.keys?.[action.key],
-            isFetching: false,
-            loading: false,
-            error: action?.error?.message,
-          },
-        },
-      };
-    default:
-      return state;
+      state.keys[action.key].isFetching = false;
+      state.keys[action.key].loading = false;
+      state.keys[action.key].error = action?.error?.message;
+      return;
   }
 };
